f02d3fe6119d23538616a2868a582712edd9a18c
Robert Graham <robert_david_graham@yahoo.com>
2020-01-27 13:07:35 -0500

    fixed video

--- backend/whats-enc.py
+++ backend/whats-enc.py
@@ -3,11 +3,18 @@ import base64;
 import urllib2;
 from Crypto.Cipher import AES;
 
-mediaK="krk2Wig1NNFPZYSBQ0gyuop3Jn2TtjfxEN+XJTefLtA="
+mediaurl = "https://mmg-fna.whatsapp.net/d/f/AsnGB7gNh6Yw52MScbJyTRMo3NCmzMpesUIYyFmEZ0lR.enc"
+mediaK = "TKgNZsaEAvtTzNEgfDqd5UAdmnBNUcJtN7mxMKunAPw="
+seed = "WhatsApp Video Keys"
 
-mediaKeyExpanded=HKDF(base64.b64decode(mediaK),112,"WhatsApp Image Keys")
+#mediaurl = "https://mmg-fna.whatsapp.net/d/f/Ap2hVbW3Da_8idKFxKUVgS7AVbDymv55tXbDVZgCAUE-.enc"
+#mediaK="krk2Wig1NNFPZYSBQ0gyuop3Jn2TtjfxEN+XJTefLtA="
+#seed = "WhatsApp Image Keys"
+
+
+mediaKeyExpanded=HKDF(base64.b64decode(mediaK),112,seed)
 macKey=mediaKeyExpanded[48:80]
-mediaData= urllib2.urlopen("https://mmg-fna.whatsapp.net/d/f/Ap2hVbW3Da_8idKFxKUVgS7AVbDymv55tXbDVZgCAUE-.enc").read()
+mediaData= urllib2.urlopen(mediaurl).read()
 
 file= mediaData[:-10]
 mac= mediaData[-10:]
